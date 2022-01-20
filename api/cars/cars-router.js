617a185806d978b11e6c0080f8803ebe24f55069
jcpcabanada <jcpcabanada@gmail.com>
2022-01-19 18:33:51 -0800

    2nd

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,15 @@
-// DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    res.json('getting all cars')
+})
+router.get('/', async (req, res, next) => {
+    res.json(`getting a car by the given id ${req.params.id}`)
+})
+router.post('/', async (req, res, next) => {
+    res.json('post the created car')
+})
+
+module.exports = router
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+    {
+        vin:'lllllllllllll',
+        make: 'toyota',
+    model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin:'hhhhhhhhhhhhh',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+
+    },
+    {
+        vin:'mmmmmmmmmmmmm',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+    }
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
