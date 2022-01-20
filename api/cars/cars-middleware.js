507bb6aa7d9f796dd2edcafc601fe754b1deddb7
jcpcabanada <jcpcabanada@gmail.com>
2022-01-19 19:33:44 -0800

    4th

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,17 @@
-const checkCarId = (req, res, next) => {
-    next()
+const Car = require('./cars-model')
+
+const checkCarId = async (req, res, next) => {
+    try {
+        const car = await Car.getById(req.params.id)
+        if(!car){
+            next({ status: 404, message: "ID not found!"})
+        } else {
+            req.car = car
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
 }
 
 const checkCarPayload = (req, res, next) => {
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -5,7 +5,7 @@ const {
     checkCarId,
 } = require('./cars-middleware')
 
-router.get('/', checkCarId, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
     try {
     const cars = await Car.getAll()
         res.json(cars)
@@ -13,14 +13,8 @@ router.get('/', checkCarId, async (req, res, next) => {
         next(err)
     }
 })
-router.get('/:id', async (req, res, next) => {
-    try {
-    const car = await Car.getById(req.params.id)
-        res.json(car)
-    } catch (err) {
-        next(err)
-    }
-
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
 })
 router.post('/', async (req, res, next) => {
     res.json('post the created car')
