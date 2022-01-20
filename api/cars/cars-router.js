d68033513f13ee351d64eb1a3d807ea0d46fcf2e
jcpcabanada <jcpcabanada@gmail.com>
2022-01-19 19:19:42 -0800

    3rd

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,12 +1,26 @@
 const express = require('express')
-
+const Car = require('./cars-model')
 const router = express.Router()
+const {
+    checkCarId,
+} = require('./cars-middleware')
 
-router.get('/', async (req, res, next) => {
-    res.json('getting all cars')
+router.get('/', checkCarId, async (req, res, next) => {
+    try {
+    const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
 })
-router.get('/', async (req, res, next) => {
-    res.json(`getting a car by the given id ${req.params.id}`)
+router.get('/:id', async (req, res, next) => {
+    try {
+    const car = await Car.getById(req.params.id)
+        res.json(car)
+    } catch (err) {
+        next(err)
+    }
+
 })
 router.post('/', async (req, res, next) => {
     res.json('post the created car')
